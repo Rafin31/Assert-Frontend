62d2d7fb5cc3f92bb09be50de1f065d5c469f14d
Rafin31 <asifhossain976@gmail.com>
2025-04-11 14:39:56 +1000

    Skelaton updated

--- src/utils/skeleton.jsx
+++ src/utils/skeleton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Skeleton() {
     return (
-        <div className="max-w-[1450px] min-h-[50vh] flex justify-center items-center flex-wrap lg:justify-evenly gap-10">
+        <div className="mt-10 max-w-[1450px] min-h-[50vh] flex justify-center items-center flex-wrap lg:justify-evenly gap-10">
             <div className="flex w-[90%] flex-col gap-4 lg:w-72">
                 <div className="skeleton h-32 w-[full]"></div>
                 <div className="skeleton h-4 w-28"></div>
